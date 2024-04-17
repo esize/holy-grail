4aff33cb73ac74be8717452004f1e94ba1beff51
Evan Sizemore <evan@wool.homes>
2024-04-17 01:53:34 -0400

    harpoon

--- ansible/roles/pretty/files/nvim/lua/evan/lazy/init.lua
+++ ansible/roles/pretty/files/nvim/lua/evan/lazy/init.lua
@@ -4,5 +4,10 @@ return {
         "nvim-lua/plenary.nvim",
         name = "plenary"
     },
+    {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" }
+}
 
 }
