94cde2cc8b96f453cb0486d59f4d4c59b561a935
Evan Sizemore <evan@wool.homes>
2024-04-17 01:16:24 -0400

    added augroup and autocmd to init

--- ansible/roles/pretty/files/nvim/lua/evan/init.lua
+++ ansible/roles/pretty/files/nvim/lua/evan/init.lua
@@ -1,3 +1,9 @@
 require("evan.settings")
 require("evan.remap")
 require("evan.lazy_init")
+
+local augroup = vim.api.nvim_create_augroup
+local EvanGroup = augroup('Evan', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
