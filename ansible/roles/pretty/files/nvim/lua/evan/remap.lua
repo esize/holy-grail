bd531f883faa218ccde67bacbf4a8c3e3ed83504
Evan Sizemore <evan@wool.homes>
2024-04-17 02:22:30 -0400

    added comments to remappings

--- ansible/roles/pretty/files/nvim/lua/evan/remap.lua
+++ ansible/roles/pretty/files/nvim/lua/evan/remap.lua
@@ -1,13 +1,21 @@
-
+-- set the "leader" key to space
 vim.g.mapleader = " "
+
+-- use " pv" to go to explore current directory
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- when line(s) are highlighted, use J or K to move them
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- bring the next line to the end of the current line
 vim.keymap.set("n", "J", "mzJ`z")
+
+-- scroll up and down half a page
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- cursor stays in the middle while searching
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
@@ -18,37 +26,40 @@ vim.keymap.set("n", "<leader>svwm", function()
     require("vim-with-me").StopVimWithMe()
 end)
 
--- greatest remap ever
+-- paste and send current selection to void
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
+-- <leader>y will yank into the system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+-- delete current line
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
-
+-- Q is good for absolutely nothing
 vim.keymap.set("n", "Q", "<nop>")
+
+-- new tmux session
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- format buffer
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
+-- navigate quickfix list
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+
+-- navigate location list
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+-- find and replace all instances of the word the cursor is on
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- if in a bash file, <leader>x makes the file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set(
-    "n",
-    "<leader>ee",
-    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
-)
-
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
-
+-- source the current file on double space
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
